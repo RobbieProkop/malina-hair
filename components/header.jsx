f28aae28a8248ba81ffba04db5fd5c2e1484b54d
Robbie Prokop <robbieprokop@gmail.com>
2022-10-21 18:57:13 +0400

    create header

---
+++ components/header.jsx
@@ -0,0 +1,16 @@
+const header = () => {
+  return (
+    <div>
+      <h3>Creative Hair Styling</h3>
+
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Gallery</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default header;
