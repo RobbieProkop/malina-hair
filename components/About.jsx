6d15b19b1bf4ae5d769714da65222b3a63e306e8
Robbie Prokop <robbieprokop@gmail.com>
2022-10-23 11:02:09 +0400

    will add Image to about later

--- components/About.jsx
+++ components/About.jsx
@@ -1,15 +1,20 @@
+import Image from "next/image";
+
 const About = () => {
   return (
     <div>
       <h2>About Malina</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
-        distinctio ut atque natus. Optio animi fuga, veritatis praesentium
-        quisquam dolores modi dignissimos labore explicabo adipisci tempore
-        reprehenderit id quo illum! Et dolorum ipsa ut aliquid ad ratione.
-        Ipsum, tenetur fugiat, praesentium maiores officiis aut iure pariatur
-        quo nisi accusamus quos.å
-      </p>
+      <div className="flex">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+          distinctio ut atque natus. Optio animi fuga, veritatis praesentium
+          quisquam dolores modi dignissimos labore explicabo adipisci tempore
+          reprehenderit id quo illum! Et dolorum ipsa ut aliquid ad ratione.
+          Ipsum, tenetur fugiat, praesentium maiores officiis aut iure pariatur
+          quo nisi accusamus quos.å
+        </p>
+        {/* <Image src={}/> */}
+      </div>
     </div>
   );
 };
