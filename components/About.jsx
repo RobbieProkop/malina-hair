31e00ad071d3903e9ed338dec81d806442578ef2
Robbie Prokop <robbieprokop@gmail.com>
2022-10-23 10:59:11 +0400

    create dummy text for about

---
+++ components/About.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About Malina</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+        distinctio ut atque natus. Optio animi fuga, veritatis praesentium
+        quisquam dolores modi dignissimos labore explicabo adipisci tempore
+        reprehenderit id quo illum! Et dolorum ipsa ut aliquid ad ratione.
+        Ipsum, tenetur fugiat, praesentium maiores officiis aut iure pariatur
+        quo nisi accusamus quos.å
+      </p>
+    </div>
+  );
+};
+export default About;
