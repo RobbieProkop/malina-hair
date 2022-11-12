7ba3f2de978d9b9263e3b66780f937da0f1bb480
Robbie Prokop <robbieprokop@gmail.com>
2022-11-13 01:40:16 +0400

    add Dhammadevs to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,17 @@
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Copyright &copy; 2023</p>
-      <a
-        href="https://www.instagram.com/creative.hair.styling/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Instagram
-      </a>
+      <p>
+        Copyright &copy; 2023
+        <a
+          href="https://www.instagram.com/creative.hair.styling/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+        | Site by <a href="https://dhaamadevs.com">DhammaDevs</a>
+      </p>
     </div>
   );
 };
