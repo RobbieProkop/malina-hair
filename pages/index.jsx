e0addeba89a50e45f5976a3de757706bf25b3d97
Robbie Prokop <robbieprokop@gmail.com>
2022-11-13 13:41:52 +0400

    begin working on base styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import Showcase from "../components/Showcase";
 import Nav from "../components/Nav";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import About from "../components/About";
 
 export default function Home() {
