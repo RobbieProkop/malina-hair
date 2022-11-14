8336a2a1a47fcbb4dcb12e02812d947fa74d6bcc
Robbie Prokop <robbieprokop@gmail.com>
2022-11-14 22:28:16 +0400

    toggle nav on click

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,9 +7,13 @@ const Header = () => {
     document.getElementById("modal").style.display = "block";
   };
 
+  const toggleNav = () => {
+    document.querySelector("body").classList.toggle("show-nav");
+  };
+
   return (
     <header>
-      <button className="toggle" id="toggle">
+      <button className="toggle" id="toggle" onClick={toggleNav}>
         <Image src="/../public/menu.png" width="30px" height="30px" />
       </button>
 
