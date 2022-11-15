a945af94448ec57ecf75070fb38b45269958aa7e
Robbie Prokop <robbieprokop@gmail.com>
2022-11-16 00:34:34 +0400

    map through work data and display to UI

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,7 +1,27 @@
+import { resources } from "../pages/api/workData.json";
+
 const Work = () => {
+  // const getPhotos = async () => {
+  //   const res = await fetch("../pages/api/workData.json");
+  //   const data = await res.json();
+  //   return data;
+  // };
+
+  // getPhotos();
+  console.log("data", resources);
+  // resources.forEach((resource) => console.log(resource.title));
+
   return (
     <div>
-      <div>Work 1</div>
+      <div>
+        {resources.map((resource) => {
+          return (
+            <a key={resource.link} href={resource.imageUrl} target="_blank">
+              {resource.title}
+            </a>
+          );
+        })}
+      </div>
       <div>Work 2</div>
       <div>Work 3</div>
       <div>Work 4</div>
