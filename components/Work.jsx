7c9e973ab8c8903e857fb3e551add48c467b6403
Robbie Prokop <robbieprokop@gmail.com>
2022-11-16 15:25:51 +0400

    begin working on carousel

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { resources } from "../pages/api/workData.json";
 
+import { useState, useRef, useEffect } from "react";
+
 const Work = () => {
   // const getPhotos = async () => {
   //   const res = await fetch("../pages/api/workData.json");
@@ -9,16 +11,76 @@ const Work = () => {
   // };
 
   // getPhotos();
+  const movePrev = () => {};
+  const moveNext = () => {};
   return (
-    <div>
-      {resources.map((resource) => {
-        return (
-          <a key={resource.link} href={resource.imageUrl} target="_blank">
-            <Image src={resource.imageUrl} width="50px" height="50px" />
-          </a>
-        );
-      })}
-    </div>
+    <section id="work" className="carousel">
+      <h2>Check Out My Work</h2>
+      <div className="">
+        <div>
+          <button onClick={movePrev} className="prev-btn">
+            {/* CHANGE THIS LATER */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-12 w-20 -ml-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+            <span className="sr-only">Prev</span>
+          </button>
+          <button onClick={moveNext} className="next-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-12 w-20 -ml-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </button>
+        </div>
+        <div className="carousel-container" ref={carousel}>
+          {resources.map((resource, index) => {
+            return (
+              <div className="carousel-item" key={index}>
+                <a href={resource.link}>
+                  <Image
+                    src={recourse.imageUrl || ""}
+                    alt={resource.title}
+                    objectFit="contain"
+                  />
+                </a>
+                <h3>{resource.title}</h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+    // <div>
+    //   {resources.map((resource) => {
+    //     return (
+    //       <a key={resource.link} href={resource.imageUrl} target="_blank">
+    //         <Image src={resource.imageUrl} width="50px" height="50px" />
+    //       </a>
+    //     );
+    //   })}
+    // </div>
   );
 };
 export default Work;
