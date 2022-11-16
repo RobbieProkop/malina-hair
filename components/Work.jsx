f0d248af40e768fb1ceb8fee8b5906cc86fd32b4
Robbie Prokop <robbieprokop@gmail.com>
2022-11-16 15:07:46 +0400

    add images to public folder. Get images from CDN

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,23 +1,23 @@
 import Image from "next/image";
-// import { resources } from "../pages/api/workData.json";
+import { resources } from "../pages/api/workData.json";
 
 const Work = () => {
-  const getPhotos = async () => {
-    const res = await fetch("../pages/api/workData.json");
-    const data = await res.json;
-    console.log("data", data);
-  };
+  // const getPhotos = async () => {
+  //   const res = await fetch("../pages/api/workData.json");
+  //   const data = await res.json;
+  //   console.log("data", data);
+  // };
 
-  getPhotos();
+  // getPhotos();
   return (
     <div>
-      {/* {resources.map((resource) => {
+      {resources.map((resource) => {
         return (
           <a key={resource.link} href={resource.imageUrl} target="_blank">
             <Image src={resource.imageUrl} width="50px" height="50px" />
           </a>
         );
-      })} */}
+      })}
     </div>
   );
 };
