487feb05a34fd64b85ce475a11c21464057945fe
Robbie Prokop <robbieprokop@gmail.com>
2022-11-17 13:57:17 +0400

    add about text

--- components/About.jsx
+++ components/About.jsx
@@ -3,15 +3,20 @@ import styles from "../styles/Home.module.scss";
 const About = () => {
   return (
     <div id="about" className={styles.about}>
-      <h2>About Malina</h2>
+      <h2>My name is Malina</h2>
       <div className="flex">
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
-          distinctio ut atque natus. Optio animi fuga, veritatis praesentium
-          quisquam dolores modi dignissimos labore explicabo adipisci tempore
-          reprehenderit id quo illum! Et dolorum ipsa ut aliquid ad ratione.
-          Ipsum, tenetur fugiat, praesentium maiores officiis aut iure pariatur
-          quo nisi accusamus quos.å
+          I believe hair is an extension of your personality. It is a part of
+          your image and signature. My purpose as a hairstylist is to help
+          enhance the locks you are born with. A little sprinkle of magic from
+          hair fairy and trust, we can achieve your hair goals. It is important
+          for me that you can maintain your hairstyle whether I&apos;m there or
+          not. So my aim is to send you out the door with tools and hair
+          knowledge to set you up for hair success. Every visit is guaranteed
+          with personalized care, from the beginning to end, you will leave with
+          confidence and satisfaction. Can&apos;t wait to meet you. Malina
+          Nguyen Phone (403)467-4464 Email Malina@creativehairstyling.co
+          Instagram @creative.hair.styling Facebook creative hairstyling
         </p>
       </div>
     </div>
