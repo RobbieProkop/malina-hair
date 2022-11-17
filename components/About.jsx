1c1bdf699a2425f2e1f3c6180412f7db3240679a
Robbie Prokop <robbieprokop@gmail.com>
2022-11-17 14:21:06 +0400

    about section styling

--- components/About.jsx
+++ components/About.jsx
@@ -1,9 +1,9 @@
-import styles from "../styles/Home.module.scss";
-
 const About = () => {
   return (
-    <div id="about" className={styles.about}>
-      <h2>My name is Malina</h2>
+    <div id="about" className="about">
+      <h2>
+        My name is <span>Malina</span>
+      </h2>
       <div className="flex">
         <p>
           I believe hair is an extension of your personality. It is a part of
@@ -14,10 +14,11 @@ const About = () => {
           not. So my aim is to send you out the door with tools and hair
           knowledge to set you up for hair success. Every visit is guaranteed
           with personalized care, from the beginning to end, you will leave with
-          confidence and satisfaction. Can&apos;t wait to meet you. Malina
-          Nguyen Phone (403)467-4464 Email Malina@creativehairstyling.co
-          Instagram @creative.hair.styling Facebook creative hairstyling
+          confidence and satisfaction. Can&apos;t wait to meet you.
         </p>
+        {/* <p>Malina
+          Nguyen Phone (403)467-4464 Email Malina@creativehairstyling.co
+          Instagram @creative.hair.styling Facebook creative hairstyling</p> */}
       </div>
     </div>
   );
