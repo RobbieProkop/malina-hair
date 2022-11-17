6998198eb2bde83a78385af337835e0b5094af02
Robbie Prokop <robbieprokop@gmail.com>
2022-11-17 23:45:45 +0400

    make images appear properly. add func to carousel

--- components/Work.jsx
+++ components/Work.jsx
@@ -13,18 +13,63 @@ const Work = () => {
   // getPhotos();
 
   // follow along for carousel at https://robkendal.co.uk/blog/how-to-build-a-multi-image-carousel-in-react-and-tailwind
-  const movePrev = () => {};
-  const moveNext = () => {};
-
   const maxScrollWidth = useRef(0);
   const [currentIndex, setCurrentIndex] = useState(0);
   const carousel = useRef(null);
+
+  const movePrev = () => {
+    if (currentIndex >= 0) {
+      setCurrentIndex((prevState) => prevState - 1);
+    }
+    console.log("currentIndex", currentIndex);
+  };
+
+  const moveNext = () => {
+    if (
+      carousel.current !== null &&
+      carousel.current.offsetWidth * currentIndex <= maxScrollWidth.current
+    ) {
+      setCurrentIndex((prevState) => prevState + 1);
+    }
+  };
+
+  const isDisabled = (direction) => {
+    if (direction === "prev") {
+      return currentIndex <= 0;
+    }
+
+    if (direction === "next" && carousel.current !== null) {
+      return (
+        carousel.current.offsetWidth * currentIndex >= maxScrollWidth.current
+      );
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    console.log("currentIndex :>> ", currentIndex);
+    maxScrollWidth.current = carousel.current
+      ? carousel.current.scrollWidth - carousel.current.offsetWidth
+      : 0;
+  }, []);
+
+  useEffect(() => {
+    if (carousel !== null && carousel.current !== null) {
+      carousel.current.scrollLeft = carousel.current.offsetWidth * currentIndex;
+    }
+  }, [currentIndex]);
+
   return (
     <section id="work" className="carouselStyle">
       <div className="heading">
         <h2>Check Out My Work</h2>
         <div className="carousel-btn">
-          <button onClick={movePrev} className="prev-btn">
+          <button
+            onClick={movePrev}
+            className="prev-btn"
+            disabled={isDisabled("prev")}
+          >
             {/* CHANGE THIS LATER */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -42,7 +87,11 @@ const Work = () => {
             </svg>
             <span className="sr-only">Prev</span>
           </button>
-          <button onClick={moveNext} className="next-btn">
+          <button
+            onClick={moveNext}
+            className="next-btn"
+            disabled={isDisabled("next")}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-12 w-20 -ml-5"
