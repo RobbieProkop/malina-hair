a91943d05f6d8b5a414e40a8be4d7ac06e38a9cc
Robbie Prokop <robbieprokop@gmail.com>
2022-11-19 16:48:07 +0400

    begin contact

---
+++ components/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className="flex-center column">
+        <h2>Interested in experiencing it for yourself?</h2>
+        <h3>Feel free to contact me on </h3>
+      </div>
+    </section>
+  );
+};
+export default Contact;
