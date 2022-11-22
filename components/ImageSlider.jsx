f071a630a91faab27a0d2e0707c7ace276c6b6c4
Robbie Prokop <robbieprokop@gmail.com>
2022-11-22 14:14:55 +0400

    make image slider with setTimeout

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -4,11 +4,31 @@ import { useState } from "react";
 const ImageSlider = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const movePrev = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const moveNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  setTimeout(() => {
+    moveNext();
+  }, 3000);
+
   return (
     <div className="slideStyles">
       {/* buttons */}
-      <div className="image-btn btn-left">❰</div>
-      <div className="image-btn btn-right">❱</div>
+      <div className="image-btn btn-left" onClick={movePrev}>
+        ❰
+      </div>
+      <div className="image-btn btn-right" onClick={moveNext}>
+        ❱
+      </div>
 
       {/* image */}
       <a href={slides[currentIndex].link}>
