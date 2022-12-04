a39a4b5b757c5588239db95c83ce35d4cdc4b393
Robbie Prokop <robbieprokop@gmail.com>
2022-12-04 18:23:50 +0400

    write basic tests

--- components/imageSlider.test.jsx
+++ components/imageSlider.test.jsx
@@ -1,15 +1,15 @@
 // const { moveNext, movePrev } = require("../components/ImageSlider");
 
-import { render, screen } from "@testing-library/react";
-import ImageSlider from "./ImageSlider";
+import { render, screen, prettyDOM } from "@testing-library/react";
+import ImageSlider, { moveNext } from "./ImageSlider";
 import data from "../pages/api/workData.json";
 
 describe("image slider component", () => {
   it("should render the imageSlider component", () => {
     render(<ImageSlider slides={data.resources} />);
   });
-  // it("should move to next image", () => {
-  //   const next = moveNext();
-  //   expect(next).toHaveBeenCalled();
-  // });
+  it("should find the left btn", () => {
+    render(<ImageSlider slides={data.resources} />);
+    expect(screen.getByTestId("test-btn-left")).toBeInTheDocument;
+  });
 });
