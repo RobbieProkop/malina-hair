7d7c9a0d2576293c592841b80da706b25912d172
Robbie Prokop <robbieprokop@gmail.com>
2022-12-04 18:00:42 +0400

    get jest working

--- components/imageSlider.test.jsx
+++ components/imageSlider.test.jsx
@@ -2,12 +2,11 @@
 
 import { render, screen } from "@testing-library/react";
 import ImageSlider from "./ImageSlider";
+import data from "../pages/api/workData.json";
 
 describe("image slider component", () => {
   it("should render the imageSlider component", () => {
-    render(ImageSlider);
-
-    screen.debug();
+    render(<ImageSlider slides={data.resources} />);
   });
   // it("should move to next image", () => {
   //   const next = moveNext();
--- jest.config.js
+++ jest.config.js
@@ -7,3 +7,12 @@
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({ dir: "." });
+
+const customJestConfig = {
+  testEnvironment: "jsdom",
+  clearMocks: true,
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+};
+
+module.exports = createJestConfig(customJestConfig);
