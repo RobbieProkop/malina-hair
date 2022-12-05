95809eb2da4ef0514b9f9eb18951aaca36025b87
Robbie Prokop <robbieprokop@gmail.com>
2022-12-05 18:50:12 +0400

    continue navbar tests

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -7,4 +7,14 @@ describe("it should navigate to the navbar and click on links", () => {
   it("should find the nav in view", () => {
     cy.get(".Nav_nav__bbBcN").should("be.visible");
   });
+  it("should find, click, and navigate to ", () => {
+    cy.get('[data-text="Contact"] > a').click();
+    cy.get('[data-text="Contact"] > a').should("be.visible");
+  });
+  // it("should find the nav in view", () => {
+  //   cy.get(".Nav_nav__bbBcN").should("be.visible");
+  // });
+  // it("should find the nav in view", () => {
+  //   cy.get(".Nav_nav__bbBcN").should("be.visible");
+  // });
 });
