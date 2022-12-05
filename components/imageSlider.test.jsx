d1598d2fc747ae6d99957e89a08c7576cab409e1
Robbie Prokop <robbieprokop@gmail.com>
2022-12-05 12:12:23 +0400

    add before each and coverage to package.json

--- components/imageSlider.test.jsx
+++ components/imageSlider.test.jsx
@@ -1,17 +1,18 @@
 // const { moveNext, movePrev } = require("../components/ImageSlider");
 
-import { render, screen, prettyDOM } from "@testing-library/react";
-import ImageSlider, { moveNext } from "./ImageSlider";
+import { render, screen } from "@testing-library/react";
+import ImageSlider from "./ImageSlider";
 import data from "../pages/api/workData.json";
 
 describe("image slider component", () => {
-  it("should render the imageSlider component", () => {
+  beforeEach(() => {
     render(<ImageSlider slides={data.resources} />);
   });
-  it("should find the left btn", () => {
-    render(<ImageSlider slides={data.resources} />);
+
+  // it("should render the imageSlider component", () => {
+  //   render(<ImageSlider slides={data.resources} />);
+  // });
+  it("should find the left btn and click it", () => {
     screen.getByTestId("test-btn-left").click;
-    const next = moveNext;
-    console.log();
   });
 });
